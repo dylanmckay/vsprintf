fa9a307e3043a972501b3157323ed8a9973ad45a
Jim Newsome <jnewsome@torproject.org>
2021-06-15 15:47:18 -0500

    Be robust to changing buffer length requirement
    vsnprintf *should* return the exact number of characters that will be
    printed with the given arguments. In Shadow, I've seen at least one
    instance of it wanting more space on the second call, though. This could
    be a bug in Shadow (maybe another thread mutating one of the arguments
    in between calls, but is difficult to debug.
    
    Probably better to be robust here and just reallocate if needed than to
    crash.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 extern crate libc;
 
 use libc::size_t;
-use std::convert::TryFrom;
 use std::io::Error;
 use std::os::raw::*;
 
@@ -16,40 +15,38 @@ pub unsafe fn vsprintf<V>(format: *const c_char, va_list: *mut V) -> Result<Stri
         .map(|bytes| String::from_utf8(bytes).expect("vsprintf result is not valid utf-8"))
 }
 
-/// Return a buffer with the given length, filled with arbitrary data.
-fn uninitd_buffer_of_len(len: usize) -> Vec<u8> {
-    let mut buffer = Vec::with_capacity(len);
-    // SAFETY: Any bit pattern is a valid u8.
-    unsafe { buffer.set_len(len) };
-    buffer
-}
-
 /// Prints a format string into a list of raw bytes that form
 /// a null-terminated C string.
 pub unsafe fn vsprintf_raw<V>(format: *const c_char, va_list: *mut V) -> Result<Vec<u8>> {
     let list_ptr = va_list as *mut c_void;
 
-    let character_count = vsnprintf_wrapper(std::ptr::null_mut(), 0, format, list_ptr);
+    let mut buffer: Vec<u8> = Vec::new();
+    loop {
+        let rv = vsnprintf_wrapper(buffer.as_mut_ptr(), buffer.len(), format, list_ptr);
 
-    // Check for errors.
-    if character_count == -1 {
-        // C does not require vsprintf to set errno, but POSIX does.
-        //
-        // Default handling will just generate an 'unknown' IO error
-        // if no errno is set.
-        return Err(Error::last_os_error());
+        // Check for errors.
+        let character_count = if rv < 0 {
+            // C does not require vsprintf to set errno, but POSIX does.
+            //
+            // Default handling will just generate an 'unknown' IO error
+            // if no errno is set.
+            return Err(Error::last_os_error());
+        } else {
+            rv as usize
+        };
+
+        if character_count >= buffer.len() {
+            let new_len = character_count + 1;
+            buffer.reserve_exact(new_len - buffer.len());
+            // SAFETY: Any bit pattern is a valid u8, and we reserved the space.
+            buffer.set_len(new_len);
+            continue;
+        }
+
+        // Drop NULL byte and any excess capacity.
+        buffer.truncate(character_count);
+        break;
     }
-
-    // Include space for NULL byte.
-    let mut buffer = uninitd_buffer_of_len(usize::try_from(character_count).unwrap() + 1);
-
-    let final_character_count =
-        vsnprintf_wrapper(buffer.as_mut_ptr(), buffer.len(), format, list_ptr);
-
-    assert_eq!(final_character_count, character_count);
-
-    // Drop null byte
-    assert_eq!(buffer.pop(), Some(0u8));
 
     Ok(buffer)
 }
